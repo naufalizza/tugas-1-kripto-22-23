5d3def5b7b127a03a4e08d66065f694be93a2a06
naufalizza <box.naufalizza@gmail.com>
2023-01-25 17:26:05 +0700

    feat: A.1 | decrypt_vigenere

--- script.js
+++ script.js
@@ -25,7 +25,24 @@ function encrypt_vigenere(p,k){
     }
     return c
 }
+function decrypt_vigenere(c,k){
+    c = c.replace(' ', "")
+    console.log(c.length)
+    console.log(k.length)
+    let p = "";
+    for (let c_idx=0; c_idx<c.length; c_idx++){
+        const k_idx = c_idx%k.length;
+        const current_c_num = char2num(c[c_idx]);
+        const current_k_num = char2num(k[k_idx]);
+        const current_p_num = (current_c_num-current_k_num+26)%26
+        p += num2char(current_p_num);
+        console.log(c[c_idx], "><", k[k_idx], "==>", num2char(current_p_num))
+    }
+    return p
+}
+
 
 let c = encrypt_vigenere(plaintext, key)
 console.log(c)
+console.log(decrypt_vigenere(c, key))
 
